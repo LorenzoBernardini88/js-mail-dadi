e500b4c31b2eb501fddade7c1eeb7914642e4039
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-13 16:51:03 +0100

    esercizio lancio dadi .

---
+++ dadi/myscript.js
@@ -0,0 +1,26 @@
+const dadoComp = [];
+
+const dadoUser = parseInt(prompt("Scegli un Numero da 1 a 6"));
+
+dadoComp.push(Math.floor(Math.random()*5+1));
+
+for(i=1; i<=dadoUser; i++){ 
+
+    if(dadoUser > 6){
+
+        console.log("ERRORE");
+
+    }else if(dadoUser < dadoComp){
+
+        console.log("Vince il Computer!" + dadoComp);
+    
+    }else if (dadoUser > dadoComp)
+    {
+        console.log("Vince L'utente!" + dadoComp);
+    
+    }else{
+        dadoUser == dadoComp;
+        console.log("Pareggio");
+    }
+    
+}
