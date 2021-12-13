377b84c7a6fa52e6258cdbdf4f0636cf1e6a039d
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2021-12-13 16:57:13 +0100

    indentazione

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -1,12 +1,12 @@
 const dadoComp = [];
 
-const dadoUser = parseInt(prompt("Scegli un Numero da 1 a 6"));
+const dadoUser = parseInt(prompt("Scegli un Numero da 1 a 6")); 
 
-dadoComp.push(Math.floor(Math.random()*5+1));
+dadoComp.push(Math.floor(Math.random()*5+1)); //contenuto Array 
 
 for(i=1; i<=dadoUser; i++){ 
 
-    if(dadoUser > 6){
+    if(dadoUser > 6){ 
 
         console.log("ERRORE");
 
@@ -14,11 +14,12 @@ for(i=1; i<=dadoUser; i++){
 
         console.log("Vince il Computer!" + dadoComp);
     
-    }else if (dadoUser > dadoComp)
-    {
+    }else if (dadoUser > dadoComp){
+
         console.log("Vince L'utente!" + dadoComp);
-    
+
     }else{
+
         dadoUser == dadoComp;
         console.log("Pareggio");
     }
